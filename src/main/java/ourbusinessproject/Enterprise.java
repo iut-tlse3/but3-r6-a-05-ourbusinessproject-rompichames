15371e59194c1d72aa0f31aa5b895dce57db1471
hrl4887a <allan.hoarau@etu.iut-tlse3.fr>
2026-01-27 11:51:32 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -2,10 +2,20 @@ package ourbusinessproject;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+//Introduction JPA
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
     @NotBlank
     String name;
     @Size(min = 16)
@@ -17,6 +27,14 @@ public class Enterprise {
     String contactEmail;
 
     public Enterprise() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name){
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -2,7 +2,17 @@ package ourbusinessproject;
 
 import jakarta.validation.constraints.NotBlank;
 
+//Introduction JPA
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     @NotBlank
     String title;
     String description;
